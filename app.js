e7fd729ce678ae094b209c8a117fdab5b94cb745
CouchMeka <meka.daniels1@gmail.com>
2023-01-26 14:31:10 -0500

    more changes to error

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const favoriteMovies = favoriteMovieList.map((movie) =>{
           })
           return
         }
-        if (req.body.starRating === undefined || typeof(req.body.starRating) !== 'number' || req.body.starRating >= 5 ){
+        if (req.body.starRating === undefined || typeof(req.body.starRating) !== 'number' || req.body.starRating >=6 ){
           res.json({
             success: false,
             message: "star rating must be a valid integer and must be a number 1 --> 5"
